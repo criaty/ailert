1a6ea9d82752baa9cc355484fd8ba361f3002981
Marcos Pereira <mluiz.pereira@gmail.com>
2024-08-04 18:32:33 -0300

    Add firebase-criaty-ailert-functions

---
+++ apps/firebase/criaty-ailert-functions/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'firebase-criaty-ailert-functions',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/firebase/criaty-ailert-functions',
+};
