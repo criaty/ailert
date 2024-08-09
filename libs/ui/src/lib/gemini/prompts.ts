755aee26ccb6476b9d18677f5d7783eb47c81d8b
Marcos Pereira <mluiz.pereira@gmail.com>
2024-08-09 08:20:19 -0300

    Adjust prompt

--- libs/ui/src/lib/gemini/prompts.ts
+++ libs/ui/src/lib/gemini/prompts.ts
@@ -2,9 +2,12 @@ import { Alert } from '@ailert/model-types';
 
 export const promptFromAlert = (alert: Alert): string => {
   const prompt = `
-Check the image for "${alert.contextToWatch}".
-Respond in ${alert.outputType} format as in the example:
-${alert.outputExample}
+Check the image and find the risk for "${alert.contextToWatch}".
+Respond in JSON format:
+{
+  "risk": "", // LOW, MEDIUM, or HIGH in uppercase
+  "message": "" // ${alert.outputMessage}
+}
   `;
   return prompt;
 };
