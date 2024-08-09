5b48e8c7c0281a650cafd0d9b4a260aa845f950b
Marcos Pereira <mluiz.pereira@gmail.com>
2024-08-09 08:20:06 -0300

    Configure model to output JSON

--- libs/ui/src/lib/gemini/ModelProvider.tsx
+++ libs/ui/src/lib/gemini/ModelProvider.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect, useState } from 'react';
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
 import { ModelContext } from './ModelContext';
 
@@ -6,18 +6,48 @@ export const ModelProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [model, setModel] = useState<GenerativeModel>();
   const [updateInterval, setUpdateInterval] = useState<number>();
 
+  const setApiKey = useCallback((apiKey: string) => {
+    const generationConfig = {
+      temperature: 0.3,
+      // topK: 0.2,
+      // topP: 0.2,
+      maxOutputTokens: 2048,
+      response_mime_type: 'application/json',
+    };
+
+    // const safetySettings = [
+    //   {
+    //     category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    //   },
+    //   {
+    //     category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    //   },
+    //   {
+    //     category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    //   },
+    //   {
+    //     category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    //     threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    //   },
+    // ];
+
+    const genAI = new GoogleGenerativeAI(apiKey);
+    const model = genAI.getGenerativeModel({
+      model: 'gemini-1.5-flash',
+      generationConfig,
+    });
+    setModel(model);
+  }, []);
+
   useEffect(() => {
     const apiKey = sessionStorage.getItem('apiKey');
     apiKey && setApiKey(apiKey);
     const updateInterval = sessionStorage.getItem('updateInterval');
     updateInterval && setUpdateInterval(parseInt(updateInterval));
-  }, []);
-
-  const setApiKey = (apiKey: string) => {
-    const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
-    setModel(model);
-  };
+  }, [setApiKey]);
 
   return (
     <ModelContext.Provider
