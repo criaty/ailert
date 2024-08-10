ad5c7f29762041998be6e622d6cc6b37cc6b0261
Marcos Pereira <mluiz.pereira@gmail.com>
2024-08-10 16:31:47 -0300

    Set example webhook to prod

--- libs/model-types/src/lib/data/Alert.ts
+++ libs/model-types/src/lib/data/Alert.ts
@@ -73,9 +73,9 @@ export const HAPPY_PERSON_ALERT: Alert = {
   imageUrl: '/images/happy-persons-alert.webp',
   contextToWatch: 'model-types:happy_person_alert_context_watch',
   outputMessage: 'model-types:happy_person_alert_output_message',
-  webhookUrl: 'http://localhost:5001/criaty-ailert/us-central1/webhookExample',
-  // webhookUrl:
-  //   'https://us-central1-criaty-ailert.cloudfunctions.net/webhookExample',
+  // webhookUrl: 'http://localhost:5001/criaty-ailert/us-central1/webhookExample',
+  webhookUrl:
+    'https://us-central1-criaty-ailert.cloudfunctions.net/webhookExample',
   webhookKey: 'criaty-ailert',
   isDefault: true,
 };
