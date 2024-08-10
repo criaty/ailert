21cadb5b0b4a5331ee58f69e65ca37dd51929c9f
Marcos Pereira <mluiz.pereira@gmail.com>
2024-08-10 17:57:17 -0300

    Fix AlertFetch on signout

--- libs/ui/src/lib/alert/AlertFetch.tsx
+++ libs/ui/src/lib/alert/AlertFetch.tsx
@@ -1,14 +1,17 @@
 import { PropsWithChildren, useEffect } from 'react';
 import { useGetAlerts } from './useGetAlerts';
+import { useCurrentCustomer } from '../hooks';
 
 export const AlertFetch: React.FC<PropsWithChildren> = ({ children }) => {
+  const [customer] = useCurrentCustomer();
   const { getAlerts } = useGetAlerts();
 
   // Get user defined alerts once.
   useEffect(() => {
+    if (!customer?.id) return;
     getAlerts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [customer]);
 
   return children;
 };
